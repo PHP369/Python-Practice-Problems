c30ab2f5b736db89eb44b15a3cf2da3c5a2943b4
Poojan Patel <patelpoojan308@gmail.com>
2022-09-30 23:18:37 +0530

    New Practice

---
+++ September 2022/024. Multiples of 3 or 5.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#Question Link: https://projecteuler.net/problem=1
+
+'''
+Question:
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+'''
+
+total = 0
+
+for x in range(1,1000):
+    if (x % 3 == 0) or (x % 5 == 0):
+        total += x
+
+print("The sum of all the multiples of 3 or 5 below 1000 is", total)
+
