b5d5943083903e2f1569cb52f5b777603c121b52
Poojan Patel <patelpoojan308@gmail.com>
2023-03-10 23:23:13 +0530

    Update Count_4s_in_T_Integers.py

--- Year 2023/Code Chef/Count_4s_in_T_Integers.py
+++ Year 2023/Code Chef/Count_4s_in_T_Integers.py
@@ -1,17 +1,21 @@
 #Question Link: https://www.codechef.com/problems/LUCKYFR
 #Program to count number of '4's in T integers
 
+#Reading no. of integers
 t = int(input())
-
-intList = []
 
 for x in range(t):
     count=0
+    
+    #Reading each integer
     a = int(input())
-    b = a
-    while(b>0):
-        rem = b % 10
+
+    #For each integer, counting no. of 4s
+    while(a>0):
+        rem = a % 10
         if (rem == 4):
             count += 1
-        b = b / 10
+        a = a / 10
+    
+    #Displaying the no. of 4s in each integer
     print(count)
